428ed7b4b1d25aa1a8fa63862ae413946c249629
amitbrown21 <amitbrown21@gmail.com>
2024-07-18 12:35:43 +0300

    updated the server url

--- app/src/main/java/com/example/hemi_tube/network/RetrofitClient.java
+++ app/src/main/java/com/example/hemi_tube/network/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://your-server-url/api/";
+    private static final String BASE_URL = "http://localhost:3000/api/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
